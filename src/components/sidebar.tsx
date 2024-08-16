5854a40e839f15ff080aece6f96264711e374637
Rachael <86849043+rachael-p@users.noreply.github.com>
2024-08-16 14:39:07 -0400

    Add Home navigation functionality to sidebar button

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,18 +1,21 @@
-import {
-  HomeIcon,
-  MagnifyingGlassIcon,
-} from "@radix-ui/react-icons";
-
+import { HomeIcon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 import { AddPostDialog } from "./post/add-post-dialog";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleClickHome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="flex flex-col gap-2 p-4">
-      <Button aria-label={"Home"} variant="ghost" size="sm">
+      <Button variant={"ghost"} size="sm" onClick={handleClickHome}>
         <HomeIcon className="w-5 h-5" />
       </Button>
-      <Button aria-label={"Search"} variant="ghost" size="sm">
+      <Button variant={"ghost"} size="sm">
         <MagnifyingGlassIcon className="w-5 h-5" />
       </Button>
       <AddPostDialog />
--- src/views/main-view.tsx
+++ src/views/main-view.tsx
@@ -1,8 +1,16 @@
 import Aside from "@/components/aside";
 import Feed from "@/components/feed";
 import Sidebar from "@/components/sidebar";
+import { useStore } from "@/lib/store";
+import { useEffect } from "react";
 
 const MainView = () => {
+  const clearSelectedPostId = useStore((state) => state.clearSelectedPostId);
+
+  useEffect(() => {
+    clearSelectedPostId();
+  }, []);
+
   return (
     <>
       <Sidebar />
